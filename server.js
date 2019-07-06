e4d4aefe4e513c387bbdcc03a535a7cea23514de
Denys Mohylin <dmohylin@bizico.com>
2019-07-06 22:17:05 +0300

    init

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const faker = require("faker");
+const app = express();
+const port = 3000;
+
+const articles = [];
+
+for (let i = 0; i < 100; i++) {
+  articles.push({
+    id: i,
+    title: faker.name.title(),
+    description: faker.lorem.paragraph(15),
+    img: faker.image.food(1200, 768),
+    date: faker.date.future(1)
+  });
+}
+
+app.get("/api/articles", (request, response) => {
+  response.set({
+    "Content-Type": "application/json"
+  });
+
+  response.send(articles);
+});
+
+app.get("/api/article", (request, response) => {
+  const { id } = request.query;
+  response.set({
+    "Content-Type": "application/json"
+  });
+
+  response.send(articles[+id]);
+});
+
+app.listen(port, err => {
+  if (err) {
+    return console.log("something bad happened", err);
+  }
+  console.log(`server is listening on ${port}`);
+});
