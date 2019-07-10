0327daa0c24e89caea2388c9675e70683268ae23
denys mohylin <dmohylin@bizico.com>
2019-07-10 13:32:52 +0300

    [feat] api changes

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get("/api/articles/:id", (request, response) => {
     "Content-Type": "application/json"
   });
 
-  response.json(articles[+id]);
+  response.json([articles[+id]]);
 });
 
 app.listen(port, (err) => {
