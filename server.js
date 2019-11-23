cf85bf443f3df83a39092a0ea7b9d1ed20932094
Denys Mohylin <dmohylin_ext@oneviewhealthcare.com>
2019-11-23 16:46:08 +0200

    [feat] tttf test

--- server.js
+++ server.js
@@ -126,6 +126,7 @@ app.get('/api/ifttt/v1/status', (request, response) => {
     'Access-Control-Allow-Headers': 'X-Requested-With',
     'Content-Type': 'application/json'
   });
+  ttt.push({headers: request.headers});
   if(request.headers['IFTTT-Channel-Key'] === 'nkhrLsvWqefrdbA4CiYaQIt3DSAc6UM7PlNJaM4fZ1kabHZTi-2Tnli_I1Kbg3Xw') {
     response.send(200);
   }
